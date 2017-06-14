9cd8feea335aa76122f3f6e07bda4598f930d0e7
TomekNTP <tomek532@gmail.com>
2017-06-14 09:49:32 +0200

    utworzenie proj
    jw

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import static java.lang.Math.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        boolean b = false;
+        b = a < 10 ? true : false;
+        double c = sqrt(a);
+        int d = 0;
+        System.in.read(d);
+        System.out.println("Hello World! " + c);
+
+    }
+}
